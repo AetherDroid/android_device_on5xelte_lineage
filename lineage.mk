688f7d68bcac78725dbf246685b74143cf47937b
Ray Sollium <120764243+RaySollium99@users.noreply.github.com>
2025-07-12 16:30:00 -0300

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -2,8 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 $(call inherit-product, device/samsung/on5xelte/on5xelte.mk)
-$(call inherit-product, device/samsung/universal7570-common/device-common.mk)
-$(call inherit-product, vendor/samsung/universal7570-common-vendor.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
